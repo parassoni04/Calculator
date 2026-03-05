02eded8ed4d30705f47523eaf7f2a79b03558ead
mandeep bhatt <mandeepbhatt52@gmail.com>
2026-03-05 11:24:19 +0530

    temp stored

--- digit_recognition/libs/layer.py
+++ digit_recognition/libs/layer.py
@@ -3,8 +3,8 @@ class Layer:
     def __init__(self,neuronList):
         self.layer: list[Neuron] = neuronList
     
-    def computeActivtionForLayer(self) -> list[Neuron]:
+    def computeActivtionForLayer(self) -> None:
         temp: list[float] = []
         for n in self.layer:
             temp.append(n.computeActivation())
-        return temp
+        
