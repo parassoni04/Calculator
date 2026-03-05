97ad53d443b6115f267e1ac24132bff24d308f81
parassoni04 <paras.soni2006@gmail.com>
2026-03-05 10:58:59 +0530

    add:neuron

---
+++ digit_recognition/libs/neuron.py
@@ -0,0 +1,12 @@
+from utilitites import squish
+
+class Neuron():
+    def __init__(self, weights : list[float], bias : float):
+        self.weights = weights
+        self.bias = bias
+
+    def computeActivation(self, weights : list[float], bias : float, activation : float):
+        for weight in weights:
+            weightedSum : float = (weight * self.activation) + self.bias
+
+        squish(weightedSum)
