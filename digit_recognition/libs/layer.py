a263dbe739f4309976d5a401e135de6eb15d8ec7
mandeep bhatt <mandeepbhatt52@gmail.com>
2026-03-05 10:50:37 +0530

    feat: added layer

---
+++ digit_recognition/libs/layer.py
@@ -0,0 +1,9 @@
+
+class Layer:
+    def __init__(self,neuronList):
+        self.layer:list[Neuron]=neuronList
+    
+    def computeActivtionForLayer(self)-> list[Neuron]:
+        temp:list[float]
+        for n in self.layer:
+            temp.append(n.computeActivation())
