135fdc3fdf38c0bc0b38ea02cf37b34c9b500454
parassoni04 <paras.soni2006@gmail.com>
2026-03-05 22:06:35 +0530

    Feat: updatedWeights

--- digit_recognition/libs/neuron.py
+++ digit_recognition/libs/neuron.py
@@ -11,3 +11,12 @@ class Neuron():
             weightedSum += (self.weights[i] * activations[i])
 
         return squish(weightedSum + self.bias)
+    
+    def weightsUpdation(self, weightsList: list[list[float], gradientOfCostFunction : list[float]]):
+        newWeight = 0
+        updatedWeights = []
+        for weight,gradient in zip(weightsList,gradientOfCostFunction):
+            newWeight = weight - gradient
+            updatedWeights.append(newWeight)
+
+    
