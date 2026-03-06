a34cda57e427e2ec11b281634d94731b99bdf3b3
parassoni04 <paras.soni2006@gmail.com>
2026-03-06 13:14:17 +0530

    add : computeGradient

--- digit_recognition/libs/neuron.py
+++ digit_recognition/libs/neuron.py
@@ -1,12 +1,14 @@
 try : 
-    from utilitites import squish
+    from utilitites import squish, transpose
 except:
-    from .utilitites import squish
+    from .utilitites import squish, transpose
 
 class Neuron():
     def __init__(self, weights: list[float], bias: float):
         self.weights = weights
         self.bias = bias
+        self.allWeightsGradients : list[list[float]] = []
+        self.allBiasesGradients : list[float] = []
 
     def computeActivation(self, activations: list[float]) -> float: 
         weightedSum: float = 0
@@ -15,16 +17,32 @@ class Neuron():
 
         return squish(weightedSum + self.bias)
     
-    def weightsUpdation(self, weightGradients : list[float]) -> None:
-        newWeight : float = 0
-        updatedWeights : list[float] = []
-        for weight, gradient in zip(self.weights, weightGradients):
-            newWeight = weight - gradient
-            updatedWeights.append(newWeight)
+    def computeGradient(self, delta : float, prevActivations : list[float]):
+        weightGradient : list[float] = []
+        for activation in prevActivations:
+            gradient = delta * activation
+            weightGradient.append(gradient)
 
-        self.weights = updatedWeights
+        biasGradient : float = delta
 
-    
-    def biasUpdation(self, biasGradient : float) -> None:
-        self.bias -= biasGradient
+        self.allWeightsGradients.append(weightGradient)
+        self.allBiasesGradients.append(biasGradient)
+
+    def updateWeights(self) -> None:
+        self.allWeightsGradients = transpose(self.allWeightsGradients)
+        weightGradient : list[float] = []
+        averageWeights : float = 0
+        for gradients in self.allWeightsGradients:
+            for gradient in gradients:
+                averageWeights += gradient
+            averageWeights = averageWeights/len(gradients)
+            weightGradient.append(averageWeights)
+
+        for gradient in weightGradient:
+            self.weights = self.weights - gradient
+
+    def updateBias(self) -> None:
+        for biasGradient in self.allBiasesGradients:
+            self.bias -= biasGradient
+
         
