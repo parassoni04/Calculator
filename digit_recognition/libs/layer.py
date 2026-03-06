4a184a746bb80f747872dd6d326dbd734ac360af
mandeep bhatt <mandeepbhatt52@gmail.com>
2026-03-06 11:25:45 +0530

    add compute gradient , do gradient decent

--- digit_recognition/libs/layer.py
+++ digit_recognition/libs/layer.py
@@ -1,50 +1,41 @@
-from .neuron import Neuron
+from neuron import Neuron
 class Layer:
     def __init__(self, neuronList: list[Neuron]):
         self.neurons: list[Neuron] = neuronList
         self.activations : list[float]  = []
         self.deltaList: list[float]= []
-        self.gradientList = list[list[float]] = [[]]
-        self.gradientBais: list[float] = 0
-        
-        
-    
+
     def computeActivtionForLayer(self, activations: list[float]) -> None:
         self.activations= []
         for neuron in self.neurons:
             activation: float = neuron.computeActivation(activations)
             self.activations.append(activation)
-    
+
     def computeDeltaForOutputLayer(self, costs: list[float]):
         self.deltaList: list[float] = []
         for cost, activation in zip(costs, self.activations):
-            tempResult:float = activation*(1-activation)*(2*cost)
+            tempResult: float = activation*(1-activation)*(2*cost)
             self.deltaList.append(tempResult)
-            
-    def computeDeltaForHiddenLayer(self, deltas: list[float], weightsList: list[list[float]]):
+
+    def computeDeltaForHiddenLayer(self, deltas: list[float], weightMatrix: list[list[float]]):
         self.deltaList = []
         wDSum : float = 0
-        transposeLists = [list(row) for row in zip(*weightsList)]
+        transposedWeightMatrix = [list(row) for row in zip(*weightMatrix)]
 
-        for  weights, delta in zip(transposeLists, deltas):
+        for weights in transposedWeightMatrix:
             wDSum : float = 0
-            for weight in weights:
+            for weight, delta in zip(weights, deltas):
                 wD: float = weight*delta
                 wDSum: float = wDSum + wD
             for activation in self.activations:
                 tempResult: float = activation*(1-activation)*wDSum
                 self.deltaList.append(tempResult)
-            
-    def computeGradientForLayer(self, prevActivations: list[float], learningRate: float):
-        self.gradientList = [[]]
-        self.gradientBais = []
-        tempList: list[float] = []
-        for delta in self.deltaList:
-            for activation  in prevActivations:
-                wD: float = learningRate * activation * delta
-                tempList.append(wD)
-                self.gradientList.append(tempList)
-        for delta in self.deltaList:
-            bais: float = learningRate*delta
-            self.gradientBais.append(bais)
-            
+
+    def computeGradientForLayer(self, prevActivations: list[float]):
+        for delta, neuron in zip(self.deltaList, self.neurons):
+            neuron.computeGradient(delta, prevActivations)
+
+    def doGradientDecentForLayer(self, learningRate: float):
+        for neuron in self.neurons:
+            neuron.updateWeights(learningRate)
+            neuron.updateBais(learningRate)
