fix: - bad number of deltas being generated in `Layer.computeDetlasForHiddenLayer()`, solved issue #20. - bias gradients not being normalised but instead being summed across all training samples in `Neuron.updateBias()`, solved issue #21. - `network.py/BackProp_computingDeltasForRemainingHiddenLayers` - wrong looping order, the loop should go from last second hidden layer to 0th, not the other way around.
refac:
- overhauled the whole codebase to shift from traditional python lists to numpy ndarrays.
- added step-normalisation in normalisation of weight/bias gradients, to prevent memory usage from balooning.
- added another measure of accuracy.

digit_recognition/libs/layer.py
@@ -1,47 +1,39 @@
-try :
-    from neuron import Neuron
-    from utilities import transpose
-except :
-    from .neuron import Neuron
-    from .utilities import transpose
+# external imports
+import numpy as np
+from numpy.typing import NDArray
 
 class Layer:
-    def __init__(self, neuronList: list[Neuron]):
-        self.neurons: list[Neuron] = neuronList
-        self.activations : list[float]  = []
-        self.deltaList: list[float]= []
+    preSemiAvgBatchSize: int
 
-    def computeActivtionForLayer(self, activations: list[float]) -> None:
-        self.activations= []
-        for neuron in self.neurons:
-            activation: float = neuron.computeActivation(activations)
-            self.activations.append(activation)
+    def __init__(self, neurons: NDArray[np.object_]):
+        self.neurons: NDArray[np.object_] = neurons
+        self.activations: NDArray[np.float64] = np.zeros(self.neurons.shape[0])
+        self.deltas: NDArray[np.float64] = np.zeros(self.neurons.shape[0])
 
-    def computeDeltaForOutputLayer(self, costs: list[float]):
-        self.deltaList: list[float] = []
-        for cost, activation in zip(costs, self.activations):
-            tempResult: float = activation*(1-activation)*(2*cost)
-            self.deltaList.append(tempResult)
+        self.preSemiAvgWeightGradients: NDArray[np.float64] = np.empty((self.neurons.shape[0], Layer.preSemiAvgBatchSize, self.neurons[0].weights.shape[0]), dtype=np.float64)
+        self.preSemiAvgBiasGradients: NDArray[np.float64] = np.empty((self.neurons.shape[0], Layer.preSemiAvgBatchSize), dtype=np.float64)
 
-    def computeDeltaForHiddenLayer(self, deltas: list[float], weightMatrix: list[list[float]]):
-        self.deltaList = []
-        wDSum : float = 0
-        transposedWeightMatrix = transpose(weightMatrix)
+    def computeActivtionForLayer(self, activations: NDArray[np.float64]) -> None:
+        for i in range(0, self.neurons.shape[0]):
+            self.activations[i] = self.neurons[i].computeActivation(activations)
 
-        for weights in transposedWeightMatrix:
-            wDSum : float = 0
-            for weight, delta in zip(weights, deltas):
-                wD: float = weight*delta
-                wDSum: float = wDSum + wD
-            for activation in self.activations:
-                tempResult: float = activation*(1-activation)*wDSum
-                self.deltaList.append(tempResult)
+    def computeDeltaForOutputLayer(self, errors: NDArray[np.float64]) -> None:
+        self.deltas: NDArray[np.float64] = self.activations * (1 - self.activations) * errors
 
-    def computeGradientForLayer(self, prevActivations: list[float]):
-        for delta, neuron in zip(self.deltaList, self.neurons):
-            neuron.computeGradient(delta, prevActivations)
+    def computeDeltaForHiddenLayer(self, deltas: NDArray[np.float64], weightMatrix: NDArray[np.float64]) -> None:
+        self.deltas: NDArray[np.float64] = self.activations * (1 - self.activations) * np.dot(weightMatrix.transpose(), deltas)
 
-    def doGradientDecentForLayer(self, learningRate: float):
+    def computeGradientForLayer(self, prevActivations: NDArray[np.float64], idx: int) -> None:
+        for i in range(0, self.neurons.shape[0]):
+            dw, db = self.neurons[i].computeGradient(self.deltas[i], prevActivations)
+            self.preSemiAvgWeightGradients[i, idx] = dw
+            self.preSemiAvgBiasGradients[i, idx] = db
+
+    def computeSemiAvgGradientsForLayer(self, idx: int) -> None:
+        for i in range(0, self.neurons.shape[0]):
+            self.neurons[i].setSemiAvgDwDb(self.preSemiAvgWeightGradients[i], self.preSemiAvgBiasGradients[i], idx)
+
+    def doGradientDescentForLayer(self, learningRate: float) -> None:
         for neuron in self.neurons:
             neuron.updateWeights(learningRate)
             neuron.updateBias(learningRate)

digit_recognition/libs/load_dataset.py
@@ -1,14 +1,25 @@
-from mnist import MNIST
+import numpy as np
+from numpy.typing import NDArray
 
 mnistDirectory: str = "./digit_recognition/datasets/mnist"
-data = MNIST(mnistDirectory)
-trainImgs, trainLabels = data.load_training()
-testImgs, testLabels = data.load_testing()
 
-for img in trainImgs:
-    for i in range(0, len(img)):
-        img[i] = img[i] / 255
+trainImgs: NDArray[np.float64]
+trainLabels: NDArray[np.uint8]
+testImgs: NDArray[np.float64]
+testLabels: NDArray[np.uint8]
 
-for img in testImgs:
-    for i in range(0, len(img)):
-        img[i] = img[i] / 255
+with open(mnistDirectory + "/train-images-idx3-ubyte", "rb") as file:
+    _, n, rows, columns = np.frombuffer(file.read(16), dtype=">i4")
+    trainImgs = np.frombuffer(file.read(), dtype=np.uint8).reshape(n, rows, columns) / 255
+
+with open(mnistDirectory + "/train-labels-idx1-ubyte", "rb") as file:
+    _, n = np.frombuffer(file.read(8), dtype=">i4")
+    trainLabels = np.frombuffer(file.read(), dtype=np.uint8).reshape(n)
+
+with open(mnistDirectory + "/t10k-images-idx3-ubyte", "rb") as file:
+    _, n, rows, columns = np.frombuffer(file.read(16), dtype=">i4")
+    testImgs = np.frombuffer(file.read(), dtype=np.uint8).reshape(n, rows, columns) / 255
+
+with open(mnistDirectory + "/t10k-labels-idx1-ubyte", "rb") as file:
+    _, n = np.frombuffer(file.read(8), dtype=">i4")
+    testLabels = np.frombuffer(file.read(), dtype=np.uint8).reshape(n)

digit_recognition/libs/neuron.py
@@ -1,53 +1,40 @@
-try : 
-    from utilities import squish, transpose
+# External imports
+import numpy as np
+from numpy.typing import NDArray
+
+# Custom imports
+try:
+    from utilities import squish
 except:
-    from .utilities import squish, transpose
+    from .utilities import squish
 
 class Neuron():
-    def __init__(self, weights: list[float], bias: float):
+    semiAvgBatchSize: int
+
+    def __init__(self, weights: NDArray[np.float64], bias: np.float64):
         self.weights = weights
         self.bias = bias
-        self.allWeightsGradients : list[list[float]] = []
-        self.allBiasesGradients : list[float] = []
 
-    def computeActivation(self, activations: list[float]) -> float: 
-        weightedSum: float = 0
-        for i in range(0, len(self.weights)):
-            weightedSum += (self.weights[i] * activations[i])
+        self.semiAvgWeightGradients: NDArray[np.float64] = np.empty((Neuron.semiAvgBatchSize, self.weights.shape[0]), dtype=np.float64)
+        self.semiAvgBiasGradients: NDArray[np.float64] = np.empty(Neuron.semiAvgBatchSize, dtype=np.float64)
 
-        return squish(weightedSum + self.bias)
-    
-    def computeGradient(self, delta : float, prevActivations : list[float]) -> None:
-        weightGradient : list[float] = []
-        for activation in prevActivations:
-            gradient = delta * activation
-            weightGradient.append(gradient)
+    def computeActivation(self, activations: NDArray[np.float64]) -> np.float64:
+        return squish(np.dot(self.weights, activations) + self.bias)
 
-        biasGradient : float = delta
+    def computeGradient(self, delta: np.float64, prevActivations: NDArray[np.float64]) -> tuple[NDArray[np.float64], np.float64]:
+        dw: NDArray[np.float64] = np.dot(delta, prevActivations) # basically a scalar multiplication of a 1darray
+        db: np.float64 = delta # aliasing for the sake of readability
 
-        self.allWeightsGradients.append(weightGradient)
-        self.allBiasesGradients.append(biasGradient)
+        return (dw, db)
 
-    def updateWeights(self, learningRate : float) -> None:
-        self.allWeightsGradients = transpose(self.allWeightsGradients)
-        weightGradient : list[float] = []
-        averageWeights : float = 0
-        for gradients in self.allWeightsGradients:
-            averageWeights = 0
-            for gradient in gradients:
-                averageWeights += gradient
-            averageWeights = averageWeights/len(gradients)
-            weightGradient.append(averageWeights)
+    def setSemiAvgDwDb(self, dW: NDArray[np.float64], dB: NDArray[np.float64], idx: int) -> None:
+        self.semiAvgWeightGradients[idx] = dW.mean(axis=0) # averages values along the columns in a 2darray and returns a 1darray
+        self.semiAvgBiasGradients[idx] = dB.mean()
 
-        for i in range(0, len(weightGradient)):
-            self.weights[i] = self.weights[i] + (weightGradient[i] * learningRate)
+    def updateWeights(self, learningRate: float) -> None:
+        fullyAvgWeightGradients: NDArray[np.float64] = self.semiAvgWeightGradients.mean(axis=0)
+        self.weights += np.dot(learningRate, fullyAvgWeightGradients)
 
-        self.allWeightsGradients = []
-
-    def updateBias(self, learningRate : float) -> None:
-        for biasGradient in self.allBiasesGradients:
-            self.bias = self.bias + (biasGradient * learningRate)
-
-        self.allBiasesGradients = []
-
-        
+    def updateBias(self, learningRate: float) -> None:
+        fullyAvgBiasGradient: np.float64 = self.semiAvgBiasGradients.mean()
+        self.bias += np.multiply(learningRate, fullyAvgBiasGradient)

digit_recognition/libs/utilities.py
@@ -1,27 +1,17 @@
-import math
+import numpy as np
+from numpy.typing import NDArray
 
-def squish(weightedSum: float) -> float:
-    e: float = math.e
-    exponentiatedWeightedSum: float = e ** (-1 * weightedSum)
-    activation: float = 1 / (1 + exponentiatedWeightedSum)
-
+def squish(weightedSum: np.float64) -> np.float64:
+    activation: np.float64 = 1 / (1 + np.exp(-weightedSum))
     return activation
 
-def computeCosts(expectedOutputs: list[float], computedOutputs: list[float]) -> list[float]:
-    costs: list[float] = []
-    for i in range(0, len(expectedOutputs)):
-        error: float = expectedOutputs[i] - computedOutputs[i]
-        costs.append(error)
-    
-    return costs
+def computeErrors(expectedOutputs: NDArray[np.uint8], computedOutputs: NDArray[np.float64]) -> NDArray[np.float64]:
+    return expectedOutputs - computedOutputs
 
-def transpose(matrix: list[list[float]]) -> list[list[float]]:
-    transpose: list[list[float]] = []
+def isAccurate(expectedOutputs: NDArray[np.uint8], computedOutputs: NDArray[np.float64]) -> bool:
+    for i in range(0, computedOutputs.shape[0]):
+        if computedOutputs[i] < 0.5: computedOutputs[i] = 0
+        else: computedOutputs[i] = 1
 
-    for i in range(0, len(matrix[0])):
-        transposedRow: list[float] = []
-        for j in range(0, len(matrix)):
-            transposedRow.append(matrix[j][i])
-        transpose.append(transposedRow)
-
-    return transpose
+    if np.dot(expectedOutputs, computedOutputs) == 1: return True
+    else: return False

digit_recognition/network.py
@@ -1,107 +1,114 @@
-from libs import Neuron, Layer, computeCosts
+# external imports
+import numpy as np
+from numpy.typing import NDArray
+
+# custom imports
+from libs import Neuron, Layer, computeErrors, isAccurate
 from libs import testImgs, testLabels, trainImgs, trainLabels
-import random
+
+# Hyper-parameters
+rng = np.random.default_rng(37)
+learningRate: float = 10
+trainBatchSize: int = 1000
+Layer.preSemiAvgBatchSize = 100
+Neuron.semiAvgBatchSize = trainBatchSize // Layer.preSemiAvgBatchSize
+testBatchSize: int = 40
+numEpochs: int = 500
 
 # Generating 128 Neurons with random weights and biases for 1st Hidden Layer
-hiddenLayer_1 = Layer([
-    Neuron([random.uniform(-1, 1) for i in range(0, 784)], random.uniform(-1, 1))
-    for i in range(0, 128)
-])
+hiddenLayer_1 = Layer(np.array([Neuron(rng.standard_normal(784), np.float64(rng.standard_normal())) for i in range(0, 128)], dtype=np.object_))
 
 # Generating 64 Neurons with random weights and biases for 2nd Hidden Layer
-hiddenLayer_2 = Layer([
-    Neuron([random.uniform(-1, 1) for i in range(0, 128)], random.uniform(-1, 1))
-    for i in range(0, 64)
-])
+hiddenLayer_2 = Layer(np.array([Neuron(rng.standard_normal(128), np.float64(rng.standard_normal())) for i in range(0, 64)], dtype=np.object_))
 
 # Generating 32 Neurons with random weights and biases for 3rd Hidden Layer
-hiddenLayer_3 = Layer([
-    Neuron([random.uniform(-1, 1) for i in range(0, 64)], random.uniform(-1, 1))
-    for i in range(0, 32)
-])
+hiddenLayer_3 = Layer(np.array([Neuron(rng.standard_normal(64), np.float64(rng.standard_normal())) for i in range(0, 32)], dtype=np.object_))
 
 # Generating 10 Neurons with random weights and biases for the Output Layer
-outputLayer = Layer([
-    Neuron([random.uniform(-1, 1) for i in range(0, 32)], random.uniform(-1, 1))
-    for i in range(0, 10)
-])
+outputLayer = Layer(np.array([Neuron(rng.standard_normal(32), np.float64(rng.standard_normal())) for i in range(0, 10)], dtype=np.object_))
 
 # Grouping all Hidden Layers together, to make iterations easier
-hiddenLayers: list[Layer] = [
+hiddenLayers = [
     hiddenLayer_1,
     hiddenLayer_2,
     hiddenLayer_3
 ]
 
-# Hyper-parameters
-learningRate: float = 0.001
-numTrainElements: int = 400
-numTestElements: int = 40
-numEpochs: int = 10
-
 # Training the model
 for i in range(0, numEpochs):
-    for j in range(0, numTrainElements):
-        # Feed Forward: Computing Activations for the 1st Hidden Layer
-        hiddenLayers[0].computeActivtionForLayer(trainImgs[j])
+    for j in range(0, Neuron.semiAvgBatchSize):
+        for k in range(0, Layer.preSemiAvgBatchSize):
+            # index of the training set
+            trainIdx: int = (i * trainBatchSize + j * Layer.preSemiAvgBatchSize + k) % trainImgs.shape[0]
 
-        # Feed Forward: Computing Activations for the Hidden Layers other than the 1st one
-        for k in range(1, len(hiddenLayers)):
-            hiddenLayers[k].computeActivtionForLayer(hiddenLayers[k-1].activations)
+            # Feed Forward: Computing Activations for the 1st Hidden Layer
+            hiddenLayers[0].computeActivtionForLayer(trainImgs[trainIdx].flatten())
 
-        # Feed Forward: Computing Activations for the Output Layer
-        lastHiddenLayerActivations: list[float] = hiddenLayers[len(hiddenLayers) - 1].activations
-        outputLayer.computeActivtionForLayer(lastHiddenLayerActivations)
+            # Feed Forward: Computing Activations for the Hidden Layers other than the 1st one
+            for k in range(1, len(hiddenLayers)):
+                hiddenLayers[k].computeActivtionForLayer(hiddenLayers[k-1].activations)
 
-        # Feed Forward: Computing individual costs for all neurons in Output Layer
-        expectedOutputs: list[float] = [0 for k in range(0, 10)]
-        expectedOutputs[trainLabels[j]] = 1
+            # Feed Forward: Computing Activations for the Output Layer
+            outputLayer.computeActivtionForLayer(hiddenLayers[len(hiddenLayers) - 1].activations)
 
-        costs: list[float] = computeCosts(expectedOutputs, outputLayer.activations)
+            # Feed Forward: Computing individual costs for all neurons in Output Layer
+            expectedOutputs: NDArray[np.uint8] = np.zeros(10, dtype=np.uint8)
+            expectedOutputs[trainLabels[trainIdx]] = 1
 
-        # Back Propagation: Computing Deltas for Output Layer
-        outputLayer.computeDeltaForOutputLayer(costs)
+            errors: NDArray[np.float64] = computeErrors(expectedOutputs, outputLayer.activations)
 
-        # Back Propagation: Computing Deltas for last Hidden Layer
-        outputLayerWeightMatrix: list[list[float]] = [neuron.weights for neuron in outputLayer.neurons]
-        hiddenLayers[len(hiddenLayers) - 1].computeDeltaForHiddenLayer(outputLayer.deltaList, outputLayerWeightMatrix)
+            # Back Propagation: Computing Deltas for Output Layer
+            outputLayer.computeDeltaForOutputLayer(errors)
 
-        # Back Propagation: Computing Deltas for every other Hidden Layer
-        for k in range(0, len(hiddenLayers) - 1):
-            weightsMatrix: list[list[float]] = [neuron.weights for neuron in hiddenLayers[k+1].neurons]
-            hiddenLayers[k].computeDeltaForHiddenLayer(hiddenLayers[k+1].deltaList, weightsMatrix)
+            # Back Propagation: Computing Deltas for last Hidden Layer
+            outputLayerWeightMatrix: NDArray[np.float64] = np.stack([neuron.weights for neuron in outputLayer.neurons])
+            hiddenLayers[len(hiddenLayers) - 1].computeDeltaForHiddenLayer(outputLayer.deltas, outputLayerWeightMatrix)
 
-        # Back Propagation: Computing Gradients for weights and biases of all Layers
-        hiddenLayers[0].computeGradientForLayer(trainImgs[j])
-        for k in range(1, len(hiddenLayers)):
-            hiddenLayers[k].computeGradientForLayer(hiddenLayers[k-1].activations)
-        outputLayer.computeGradientForLayer(hiddenLayers[len(hiddenLayers) - 1].activations)
+            # Back Propagation: Computing Deltas for remaining Hidden Layers
+            for k in range(len(hiddenLayers) - 2, -1, -1):
+                weightsMatrix: NDArray[np.float64] = np.stack([neuron.weights for neuron in hiddenLayers[k+1].neurons])
+                hiddenLayers[k].computeDeltaForHiddenLayer(hiddenLayers[k+1].deltas, weightsMatrix)
+
+            # Back Propagation: Computing Gradients for weights and biases of all Layers
+            hiddenLayers[0].computeGradientForLayer(trainImgs[trainIdx].flatten(), k)
+            for k in range(1, len(hiddenLayers)):
+                hiddenLayers[k].computeGradientForLayer(hiddenLayers[k-1].activations, k)
+            outputLayer.computeGradientForLayer(hiddenLayers[len(hiddenLayers) - 1].activations, k)
+        
+        # Averaging the stored weight/bias gradients prematurely to lower memory usage
+        for layer in hiddenLayers:
+            layer.computeSemiAvgGradientsForLayer(j)
 
     # Back Propagation: Adjusting all the weights and biases according to the average of gradients
     for layer in hiddenLayers:
-        layer.doGradientDecentForLayer(learningRate)
-    outputLayer.doGradientDecentForLayer(learningRate)
+        layer.doGradientDescentForLayer(learningRate)
+    outputLayer.doGradientDescentForLayer(learningRate)
 
-    # Testing: Computing accuracy in terms of SumOfSquaredErrors
-    sse: float = 0
-    for j in range(0, numTestElements):
+    # Testing: Computing accuracy in terms of SumOfSquaredErrors and CorrectPredicions
+    sse: np.float64 = np.float64(0)
+    accurateOutputs: int = 0
+
+    for j in range(0, testBatchSize):
         # Feed Forward: Computing Activations for the 1st Hidden Layer
-        hiddenLayers[0].computeActivtionForLayer(testImgs[j])
+        hiddenLayers[0].computeActivtionForLayer(testImgs[j].flatten())
 
         # Feed Forward: Computing Activations for the Hidden Layers other than the 1st one
         for k in range(1, len(hiddenLayers)):
             hiddenLayers[k].computeActivtionForLayer(hiddenLayers[k-1].activations)
 
         # Feed Forward: Computing Activations for the Output Layer
-        lastHiddenLayerActivations: list[float] = hiddenLayers[len(hiddenLayers) - 1].activations
-        outputLayer.computeActivtionForLayer(lastHiddenLayerActivations)
+        outputLayer.computeActivtionForLayer(hiddenLayers[len(hiddenLayers) - 1].activations)
 
         # Feed Forward: Computing individual costs for all neurons in Output Layer
-        expectedOutputs: list[float] = [0 for k in range(0, 10)]
+        expectedOutputs: NDArray[np.uint8] = np.zeros(10, dtype=np.uint8)
         expectedOutputs[testLabels[j]] = 1
 
-        costs: list[float] = computeCosts(expectedOutputs, outputLayer.activations)
-        for k in range(0, len(costs)):
-            sse += costs[k] ** 2
-    avg_sse: float = sse / numTestElements
-    print(f"{i}th Cost: {avg_sse}")
+        errors: NDArray[np.float64] = computeErrors(expectedOutputs, outputLayer.activations)
+        sse += np.mean(errors ** 2)
+        
+        accurate: bool = isAccurate(expectedOutputs, outputLayer.activations)
+        accurateOutputs += accurate
+
+    accuracy: float = accurateOutputs / testBatchSize
+    sse /= testBatchSize
+    print(f"[{i}th]: Cost: {sse}, Accuracy: {accuracy}")